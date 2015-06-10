cf1d17bcfc8e35750140403becac39d7ad8a2082
Torsten Feld <torsten@torsten-feld.de>
2015-06-10 21:35:20 +0200

    basic set for couchbase doc incl. test (with available couchbase server)

--- lib/logstash/outputs/couchbase.rb
+++ lib/logstash/outputs/couchbase.rb
@@ -1,17 +1,29 @@
 # encoding: utf-8
-require "logstash/outputs/base"
-require "logstash/namespace"
+require 'logstash/outputs/base'
+require 'logstash/namespace'
+require 'couchbase'
 
 # An example output that does nothing.
 class LogStash::Outputs::Couchbase < LogStash::Outputs::Base
-  config_name "couchbase"
+  config_name 'couchbase'
+
+  config :host, :validate => :array, :default => ['127.0.0.1']
+  config :bucket, :validate => :string, :required => true
+  config :key, :validate => :string, :required => true
+  config :command, :validate => ['set', 'add', 'replace'], :default => 'set'
 
   public
   def register
+    cluster = Couchbase::Cluster.new(@host)
+    @b = cluster.open_bucket(@bucket)
   end # def register
 
   public
   def receive(event)
-    return "Event received"
+    doc = Couchbase::Document.new(:id => @key, :content => event)
+    res = @b.upsert(doc)
+    puts res.inspect
+    # return 'Event received'
+    res
   end # def event
 end # class LogStash::Outputs::Couchbase
--- spec/outputs/couchbase_spec.rb
+++ spec/outputs/couchbase_spec.rb
@@ -1,21 +1,41 @@
 require "logstash/devutils/rspec/spec_helper"
-require "logstash/outputs/couchbase"
+require File.absolute_path(File.join(File.dirname(__FILE__), '../../lib/logstash/outputs/couchbase'))
+require 'couchbase'
 require "logstash/codecs/plain"
 require "logstash/event"
+require 'rspec'
+require 'rspec-expectations'
 
 describe LogStash::Outputs::Couchbase do
   let(:sample_event) { LogStash::Event.new }
-  let(:output) { LogStash::Outputs::Couchbase.new }
 
-  before do
-    output.register
-  end
+  # before do
+  #   output.register
+  # end
 
-  describe "receive message" do
-    subject { output.receive(sample_event) }
+  describe 'store document' do
+    before {
+      @key = '123'
+      @expected_output = Couchbase::Document.new(:id => @key, :content => sample_event)
+    }
+    subject {
+      # key = '123'
+      config = {
+        'host' => ['127.0.0.1'],
+        'bucket' => 'default',
+        'key' => @key
+      }
+      # expected_output = Couchbase::Document.new(:id => key, :content => sample_event)
+      puts @expected_output.inspect
+      # puts config['outputs']['couchbase']['key']
+      output = LogStash::Outputs::Couchbase.new config
+      output.register
+      output.receive(sample_event)
+    }
 
-    it "returns a string" do
-      expect(subject).to eq("Event received")
+    it 'returns a string' do
+      expect(subject).to be_a_kind_of(Couchbase::Document)
+      # expect(subject).to eq('Event received')
     end
   end
 end
